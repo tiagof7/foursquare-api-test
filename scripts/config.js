26571a6ada83c36c23aff8638dc6de40dced61ec
Tiago Ferreira <tiagof7@yahoo.com.br>
2016-06-16 23:48:53 -0300

    Inclusão das dependências do AngularJs

---
+++ scripts/config.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('services.config', [])
+  .constant('configuration', {
+    localPath: 'http://riqao.algartech.com:8080/server/',
+  });
---
+++ scripts/controllers/globalCtrl.js
@@ -0,0 +1,25 @@
+'use strict';
+
+angular.module('foursquareApiTestApp')
+    .controller('GlobalCtrl', function ($scope) {
+
+        $scope.alerts = [];
+
+        $scope.yourName = 'tiago';
+
+        
+
+        // Restangular.setErrorInterceptor(function (response) {
+        //     if (response.status == 401) {
+        //         AuthService.logout();
+        //         return false;
+        //     } else if (response.data && response.data.consumerMessage) {
+        //         growl.error($filter('translate')('message.error.' + response.data.consumerMessage));
+        //         return false;
+        //     }
+        //     return true;
+        // });
+
+
+
+    });
